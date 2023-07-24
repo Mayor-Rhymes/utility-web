7ef0b60b2af23343ff2422a40a4af7d3fcd510ec
Olakunle Raheem <olaks.raheem@gmail.com>
2023-07-24 13:50:10 +0300

    Feat: Fixed Button alignment issue

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -11,28 +11,28 @@ export default function Calculator() {
             </div>
 
             <div className="grid grid-cols-4 gap-3">
-                <div className="text-center bg-red-100 active:shadow-sm active:text-sm flex items-center justify-center rounded-md px-3 py-3 col-span-2 shadow-md"><MdClear /></div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">pi</div>
-                <div className="text-center flex items-center active:shadow-sm active:text-sm justify-center bg-slate-100 rounded-md px-3 py-3 shadow-md"><BsFillBackspaceFill /></div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">7</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">8</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">9</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">x</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">4</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">5</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">6</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">-</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">1</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">2</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">3</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">/</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">+/-</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">0</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">.</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">+</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">%</div>
-                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md">^</div>
-                <div className="text-center bg-green-100 active:shadow-sm active:text-sm rounded-md px-3 py-3 shadow-md col-span-2">=</div>
+                <div className="text-center bg-red-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center col-span-2 shadow-md"><MdClear /></div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">Pi</div>
+                <div className="text-center flex items-center active:shadow-sm active:text-sm px-3 py-3 justify-center bg-slate-100 rounded-md shadow-md"><BsFillBackspaceFill /></div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">7</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">8</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">9</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">x</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">4</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">5</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">6</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">-</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">1</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">2</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">3</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">/</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">+/-</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">0</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">.</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">+</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">%</div>
+                <div className="text-center bg-slate-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md">^</div>
+                <div className="text-center bg-green-100 active:shadow-sm active:text-sm px-3 py-3 rounded-md flex items-center justify-center shadow-md col-span-2">=</div>
                 
                 
                 
