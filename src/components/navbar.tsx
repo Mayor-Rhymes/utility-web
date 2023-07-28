ef28715d0a95c012e1afd35a8d307072babef999
Olakunle Raheem <olaks.raheem@gmail.com>
2023-07-28 17:09:41 +0300

    Chore: Changed name

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,9 +11,9 @@ export default function Navbar() {
 
   return (
     <nav className="flex flex-col space-y-4 lg:flex-row justify-around lg:h-[70px] py-4 lg:items-center px-4">
-      <h3 className="grow hidden lg:block">Not Sure</h3>
+      <h3 className="grow hidden lg:block">Ryoiki Tenkai</h3>
       <div className="lg:hidden flex justify-between w-full grow items-center">
-        <h3 className="grow">Not Sure</h3>
+        <h3 className="grow">Ryoiki Tenkai</h3>
         <RxHamburgerMenu onClick={() => setVisible(!visible)} className="lg:hidden text-blue-500 text-xl hover:text-blue-700 cursor-pointer" />
       </div>
 
