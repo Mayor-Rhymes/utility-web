42762676e6b0f46c09d5d737bc5b22bd36ca2c38
Olakunle Raheem <olaks.raheem@gmail.com>
2023-07-31 13:58:42 +0300

    Chore: Cleaned up code

--- src/components/nextauthprovider.tsx
+++ src/components/nextauthprovider.tsx
@@ -1,13 +1,11 @@
 "use client";
-import {SessionProvider} from 'next-auth/react';
-import { ReactNode } from 'react';
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
 
-
-export default function NextAuthProvider({children}: {children: ReactNode}) {
-
-     return (
-        <SessionProvider>
-            {children}
-        </SessionProvider>
-     )
+export default function NextAuthProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
