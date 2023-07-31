655d4d64987fcfaa840d772a25c17b57f3c285f9
Olakunle Raheem <olaks.raheem@gmail.com>
2023-07-31 15:17:00 +0300

    Chore: Cleaned up authentication function

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,5 @@
-import NextAuth, {NextAuthOptions} from "next-auth";
-
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: NextAuthOptions = {
-
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-  ],
-  
-};
-
-
-
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { authOptions } from "@/app/libs/auth/authOptions";
 
 const handler = NextAuth(authOptions);
 
